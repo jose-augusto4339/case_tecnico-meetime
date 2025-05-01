c7e2f8b6bf2bd5e493ab645efc4a7c3aaa571ed7
jose-augusto4339 <joseaugustoribeiro1998@gmail.com>
2025-05-01 17:01:41 -0300

    fix start-app.sh

--- start-app.sh
+++ start-app.sh
@@ -2,8 +2,8 @@
 
 # Build do projeto
 
-mvn -f /case-tecnico/pom.xml clean install
+mvn -f ./case-tecnico/pom.xml clean install
 
 # Inicialização da aplicação
 
-java -jar /case-tecnico/target/case-tecnico-0.0.1-SNAPSHOT.jar --spring.config.location=file:/application.properties
+java -jar ./case-tecnico/target/case-tecnico-0.0.1-SNAPSHOT.jar --spring.config.location=file:./application.properties
